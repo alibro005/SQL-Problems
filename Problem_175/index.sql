711179b2e2d708dab16bccea3010fa0498fb7083
alibro005 <itsalibro9@gmail.com>
2025-07-31 09:20:47 +0500

    Combine two Tables

--- Problem_175/index.sql
+++ Problem_175/index.sql
@@ -1,7 +1,8 @@
 
--- Query to retrieve first name, last name, city, and state for each person
--- Uses LEFT JOIN to include all people, even if they don't have an address
--- From LeetCode Problem 175: Combine Two Tables
+-- LeetCode Problem 175: Combine Two Tables
+-- https://leetcode.com/problems/combine-two-tables/
+-- This query retrieves each person's first and last name along with their city and state (if available)
+-- It uses a LEFT JOIN to ensure that all people are included, even if no address is found
 
 SELECT 
     p.firstName, 
