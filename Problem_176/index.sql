96439317456315fe34f09ac213a1f500c6b08534
alibro005 <itsalibro9@gmail.com>
2025-07-31 09:19:44 +0500

    Second Highest Salary

--- Problem_176/index.sql
+++ Problem_176/index.sql
@@ -1,4 +1,9 @@
 
+-- LeetCode Problem 176: Second Highest Salary
+-- https://leetcode.com/problems/second-highest-salary/
+-- This query returns the second highest distinct salary from the Employee table
+-- It filters salaries that are less than the maximum and selects the highest among them
+
 SELECT max(salary) AS SecondHighestSalary 
 FROM Employee
 WHERE salary < (select max(salary) from employee);
